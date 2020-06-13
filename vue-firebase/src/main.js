93282b414a3b0db7d0fd25d5f6fd9fd25e280e62
Marcos Andrade <marcospandrade2@gmail.com>
2020-06-13 16:35:21 -0400

    criação home page

--- vue-firebase/src/main.js
+++ vue-firebase/src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ApolloClient from "apollo-boost"
+import VueApollo from "vue-apollo"
 
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+})
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  apolloProvider
 }).$mount('#app')
